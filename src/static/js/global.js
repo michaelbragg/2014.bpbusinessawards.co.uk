9bdbb0ba27f7370cfe2a8e55c872f0a1189966ae
Michael Bragg <michael@michaelbragg.net>
2014-08-27 12:30:46 +0100

    Add FilpClickJS
    Add a countdown clock for the nomination deadline

--- src/static/js/global.js
+++ src/static/js/global.js
@@ -22,3 +22,27 @@
 {% include js/jquery.svgfallback.js %}
 
 {% include bower_components/FitVids/jquery.fitvids.js %}
+
+{% include bower_components/FlipClock/compiled/flipclock.min.js %}
+
+(function ( $ ) {
+
+  $(document).ready(function() {
+
+    // Grab the current date
+    var currentDate = new Date(),
+    // Set the closing date
+        futureDate  = new Date('2014-09-26T15:00:00'),
+    // Calculate the difference in seconds between the future and current date
+        diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+    // Instantiate a coutdown FlipClock
+        clock = $('#js-countdown').FlipClock(diff, {
+          clockFace: 'DailyCounter',
+          countdown: true,
+          showSeconds: false
+        });
+
+  });
+
+}(jQuery));
